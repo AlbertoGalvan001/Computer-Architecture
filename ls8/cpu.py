edd5c7998d1cc954161decc1817a49ceb0a4106a
AlbertoGalvan001 <54903554+AlbertoGalvan001@users.noreply.github.com>
2020-09-08 19:02:13 -0400

    unhardcode day-2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,7 @@
 """CPU functionality."""
 
 import sys
+import fileio
 
 HLT = 0b00000001
 LDI = 0b10000010
@@ -23,16 +24,8 @@ class CPU:
         address = 0
 
         # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        program = fileio.command_list
+    
 
         for instruction in program:
             self.ram[address] = instruction
@@ -92,5 +85,4 @@ class CPU:
                 value = self.reg[register]
                 print(value)
                 self.pc += 2
-
 
